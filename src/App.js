120b20dadde9e97ecb08309a15a893fbd9ea35cd
sophiehu2509 <sophie.huye@gmail.com>
2020-06-11 21:11:37 -0400

    6/10/2020

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Link, Switch, Route, NavLink} from 'react-router-dom'
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+    
+    <Router >
+      <div className="nav-center ">
+        <Link className = "nav-links" to="/"><img src="./images/logo.svg" alt=""></img></Link>
+      </div>
+    <NavLink to = '/' >
+      <ul className="nav-btn">
+          <li >
+          
+          </li>
+      </ul>
+         
+    </NavLink>
+    <Switch>
+      <Route exact path="/"><Home /></Route>
+      <Route exact path="/rooms"><Rooms /></Route>
+      <Route exact path="/rooms/singleRoom/:slug">< SingleRoom /></Route>
+      <Route exact path='' >< Error /></Route>
+    </Switch>
+    
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            This is the single Room page
+        </div>
+    )
+}
+
+export default SingleRoom
